c576dd8b1d3c7a7154de20f12f5600d94892e757
programarivm <info@programarivm.com>
2019-08-18 19:11:49 +0100

    basic example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
+import ReactDataGrid from 'react-data-grid';
 import logo from './logo.svg';
 import './App.css';
 
+const columns = [
+  { key: 'id', name: 'ID' },
+  { key: 'title', name: 'Title' },
+  { key: 'count', name: 'Count' } ];
+
+const rows = [{id: 0, title: 'row1', count: 20}, {id: 1, title: 'row1', count: 40}, {id: 2, title: 'row1', count: 60}];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactDataGrid
+      columns={columns}
+      rowGetter={i => rows[i]}
+      rowsCount={3}
+      minHeight={150}
+    />
   );
 }
 
