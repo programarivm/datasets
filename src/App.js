d2aaa393d29b5306e5d58e0e97da879656f9607c
programarivm <info@programarivm.com>
2019-08-18 19:34:35 +0100

    sample data is added

--- src/App.js
+++ src/App.js
@@ -8,15 +8,131 @@ const columns = [
   { key: 'title', name: 'Title' },
   { key: 'count', name: 'Count' } ];
 
-const rows = [{id: 0, title: 'row1', count: 20}, {id: 1, title: 'row1', count: 40}, {id: 2, title: 'row1', count: 60}];
+const rows = [
+  {
+    id: 0,
+    title: 'a',
+    count: 20
+  },
+  {
+    id: 1,
+    title: 'b',
+    count: 40
+  },
+  {
+    id: 2,
+    title: 'c',
+    count: 60
+  },
+  {
+    id: 3,
+    title: 'c',
+    count: 80
+  },
+  {
+    id: 4,
+    title: 'd',
+    count: 100
+  },
+  {
+    id: 5,
+    title: 'e',
+    count: 120
+  },
+  {
+    id: 6,
+    title: 'f',
+    count: 140
+  },
+  {
+    id: 7,
+    title: 'g',
+    count: 160
+  },
+  {
+    id: 8,
+    title: 'h',
+    count: 180
+  },
+  {
+    id: 9,
+    title: 'i',
+    count: 200
+  },
+  {
+    id: 10,
+    title: 'j',
+    count: 220
+  },
+  {
+    id: 11,
+    title: 'k',
+    count: 240
+  },
+  {
+    id: 12,
+    title: 'l',
+    count: 260
+  },
+  {
+    id: 13,
+    title: 'm',
+    count: 280
+  },
+  {
+    id: 14,
+    title: 'n',
+    count: 300
+  },
+  {
+    id: 15,
+    title: 'o',
+    count: 320
+  },
+  {
+    id: 16,
+    title: 'p',
+    count: 340
+  },
+  {
+    id: 17,
+    title: 'q',
+    count: 360
+  },
+  {
+    id: 18,
+    title: 'r',
+    count: 380
+  },
+  {
+    id: 19,
+    title: 's',
+    count: 400
+  },
+  {
+    id: 20,
+    title: 't',
+    count: 420
+  },
+  {
+    id: 21,
+    title: 'u',
+    count: 440
+  },
+  {
+    id: 22,
+    title: 'v',
+    count: 460
+  }
+];
 
 function App() {
   return (
     <ReactDataGrid
       columns={columns}
       rowGetter={i => rows[i]}
-      rowsCount={3}
-      minHeight={150}
+      rowsCount={23}
+      minHeight={300}
     />
   );
 }
